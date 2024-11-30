2181ee73b2895a884e9a2286bb252237431d5144
vitorde-sa <81320726+vitorde-sa@users.noreply.github.com>
2024-11-30 11:03:13 -0300

    a

---
+++ TCP_3_2024/Assets/Scripts/Player/estatos_do_jogador/Estatos_Do_Jogador.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estatos_Do_Jogador : MonoBehaviour
+{
+    public int vida_do_jogador;
+    public int vida_corente_do_jogador;
+
+    public int escudo_do_jogador;
+    public int escudo_corente_do_jogador;
+
+    public int dinheiro_do_jogador;
+    public int dinheiro_corente_do_jogador;
+
+
+
+
+    public void receber_dano( int dano) 
+    {
+
+    }
+ 
+}
