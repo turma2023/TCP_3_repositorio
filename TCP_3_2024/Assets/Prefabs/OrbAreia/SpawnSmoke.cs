794a4ea12a5752e049ddf09f0a67a99c19a3f5eb
Fábio de Souza <fabiofesouza@outlook.com>
2025-02-13 15:59:46 -0300

    Icone da Smoke
    Quando usa a habilidade o icone fica cinza até que possa usar novamente no proximo round

--- TCP_3_2024/Assets/Prefabs/OrbAreia/SpawnSmoke.cs
+++ TCP_3_2024/Assets/Prefabs/OrbAreia/SpawnSmoke.cs
@@ -1,48 +1,68 @@
 using UnityEngine;
 using Fusion;
+using UnityEngine.UI;
 public class SpawnSmoke : NetworkBehaviour 
 {
     private NetworkRunner runner;
     public NetworkObject smoke;
-
     public NetworkObject previewPrefab;
     public float range = 10f;
-
+    public Image IconSmoke;
+    private Color IconSmokePadrao;
     private NetworkObject previewInstance;
+    private bool used = false;
 
 
     private void Start()
     {
+        if (Object.HasInputAuthority)
+            IconSmoke.gameObject.SetActive(true);
+        
         runner = FindObjectOfType<Spawner>().Runner;
         smoke.GetComponent<ScaleSmoke>().runner = runner;
+        IconSmokePadrao = IconSmoke.color;
+
     }
 
     private void Update()
     {   
-        if (Object.HasInputAuthority)
+        if (!used)
         {
-            if (Input.GetKey(KeyCode.E))
+            if (Object.HasInputAuthority)
             {
-                Vector3 targetPosition = GetTargetPosition();
-
-                if (previewInstance == null)
+                if (Input.GetKey(KeyCode.E))
                 {
-                    previewInstance = Instantiate(previewPrefab, targetPosition, Quaternion.identity);
-                }
-                else{
-                    previewInstance.transform.position = targetPosition;
-                }
-            }
+                    Vector3 targetPosition = GetTargetPosition();
 
-            if (Input.GetKeyUp(KeyCode.E))
-            {
-                if (previewInstance != null)
+                    if (previewInstance == null)
+                    {
+                        previewInstance = Instantiate(previewPrefab, targetPosition, Quaternion.identity);
+                    }
+                    else{
+                        previewInstance.transform.position = targetPosition;
+                    }
+                }
+
+                if (Input.GetKeyUp(KeyCode.E))
                 {
-                    RPC_SpawnSmoke(previewInstance.transform.position);
-                    Destroy(previewInstance.gameObject);
+                    if (previewInstance != null)
+                    {
+                        RPC_SpawnSmoke(previewInstance.transform.position);
+                        Destroy(previewInstance.gameObject);
+                        used = true;
+                        IconSmoke.color = Color.gray;
+                    }
                 }
             }
         }
+        else{
+            Debug.LogError("não pode usar novamente");
+        }
+    }
+
+    public void ActivateAgain(){
+        used = false;
+        IconSmoke.color = IconSmokePadrao;
     }
 
 
